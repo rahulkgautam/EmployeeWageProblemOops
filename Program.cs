ec80fde66f337c0974f65991882e8fe9d2665c5c
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-16 16:16:19 +0530

    Welcome Employee Wage Problem

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpoyeeWageProblemDay8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage Problem");
+        }
+    }
+}
